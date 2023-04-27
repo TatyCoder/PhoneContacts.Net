0fea9ebb615a60cda2098577aeff0c77e5548695
Tatiana Domingue <tatiana.domingue@protonmail.com>
2023-04-27 15:29:53 -0400

    Starting up Controller

---
+++ PhoneContacts.Net/Controllers/ContactsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PhoneContacts.Net.Models;
+
+namespace PhoneContacts.Net.Controllers;
+
+[ApiController]
+[Route ("/api/contacts")]
+public class ContactsController : ControllerBase
+{
+
+    [HttpGet]
+    public List<Contact> getAllContacts ()
+    {
+        List<Contact> contacts = new List<Contact> ();
+        Contact contact = new Contact (1,"name1");
+        contacts.Add (contact);
+        return contacts;
+    }
+}
---
+++ PhoneContacts.Net/Services/ContactsService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace PhoneContacts.Net.Services
+{
+    public class ContactsService
+    {
+        public ContactsService()
+        {
+        }
+    }
+}
+
