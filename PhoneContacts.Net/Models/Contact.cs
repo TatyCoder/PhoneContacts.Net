3a01c5e7a3145fd668b2c7fdcb443b01e5a3e3a3
Tatiana Domingue <tatiana.domingue@protonmail.com>
2023-04-27 14:54:42 -0400

    first commit

---
+++ PhoneContacts.Net/Models/Contact.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace PhoneContacts.Net.Models;
+
+    public class Contact
+    {
+        public int contactId { get; set; }
+        public String name { get; set; }
+
+        public Contact(int contactId, String name)
+        {
+            this.contactId = contactId;
+            this.name = name;
+        }
+
+    }
---
+++ PhoneContacts.Net/Models/Phone.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Xml.Linq;
+
+namespace PhoneContacts.Net.Models;
+
+public class Phone
+{
+    public int phoneId { get; set; }
+    public String phone { get; set; }
+    public int contactId { get; set; }
+
+    public Phone(int phoneId, String phone, int contactId)
+    {
+        this.phoneId = phoneId;
+        this.phone = phone;
+        this.contactId = contactId;
+    }
+}
+
